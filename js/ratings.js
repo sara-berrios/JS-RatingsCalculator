87157ae3ffedf40394b97d28a6e1fc4e79e70197
sara berrios <sara-berrios@pluralsight.com>
2021-07-13 16:22:06 -0600

    script fixed

--- js/ratings.js
+++ js/ratings.js
@@ -19,5 +19,5 @@ function collect_ratings(){
 }
 document.addEventListener('change', () => {
     const ratings = collect_ratings();
-    document.querySelector('average').value = (ratings.average).toFixed(2);
+    document.querySelector('#average').value = (ratings.average).toFixed(2);
 });
