fac87bee678eac0abd927c7473ee3d168c0db0e0
sara berrios <sara-berrios@pluralsight.com>
2021-07-13 16:08:42 -0600

    wip

---
+++ js/ratings.js
@@ -0,0 +1,23 @@
+function collect_ratings(){
+    const ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    };
+
+    let rating = 0;
+    const elements = document.querySelectorAll("rating");
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace("star", ""));
+        ratings.count += parseInt(element.value);
+        ratings.sum += ratings.count * rating;
+    })
+    if(ratings.count !== 0){
+        ratings.average += (ratings.sum / ratings.count).toFixed(2);
+    }
+    return ratings;
+}
+document.addEventListener('change', () => {
+    const ratings = collect_ratings();
+    document.querySelector('average').value = ratings.average;
+});
