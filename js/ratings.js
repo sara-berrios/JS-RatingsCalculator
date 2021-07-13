274a46daeb75ca2db154c41d3858091080019f20
sara berrios <sara-berrios@pluralsight.com>
2021-07-13 16:20:48 -0600

    script fixed

--- js/ratings.js
+++ js/ratings.js
@@ -6,11 +6,11 @@ function collect_ratings(){
     };
 
     let rating = 0;
-    const elements = document.querySelectorAll("rating");
+    const elements = document.querySelectorAll(".rating");
     elements.forEach((element) => {
         rating = parseInt(element.id.replace("star", ""));
         ratings.count += parseInt(element.value);
-        ratings.sum += ratings.count * rating;
+        ratings.sum += parseInt(element.value) * rating;
     })
     if(ratings.count !== 0){
         ratings.average += ratings.sum / ratings.count;
